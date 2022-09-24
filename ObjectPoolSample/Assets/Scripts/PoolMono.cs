7876f828259b2274c8a147f3e8e66209e9b36810
NikitaHlinsky-0308 <75807378+NikitaHlinsky-0308@users.noreply.github.com>
2022-09-24 18:22:06 +0300

    Initial commit

---
+++ ObjectPoolSample/Assets/Scripts/PoolMono.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolMono<T> where T : MonoBehaviour
+{
+    public T prefab { get; }
+    public bool autoExpand { get; set; }
+    public Transform container { get; }
+
+    private List<T> pool;
+
+}
